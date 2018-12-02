57d54f3e640cba87c64dadda44134cce54cd09b2
Uko Melis <ukomelis56@gmail.com>
2018-12-02 17:19:05 +0200

    api hehe

---
+++ FootyAPI/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootyAPI.Controllers
+{
+    [Route("api/player")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "ur mom";
+        }
+    }
+}
